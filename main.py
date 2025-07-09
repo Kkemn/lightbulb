69b4fe3071342228515aeba6c3e9ae59a66b42fb
Kamil Kemnitz <kamil.kemnitz@gmail.com>
2025-07-09 15:56:31 +0200

    Added conf file handling with conf loading at start

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from datetime import datetime
 import requests
 from tkinter import *
 from tkinter import ttk
+import json
 
-BULB_IP = "192.168.0.15"
+BULB_IP = "192.168.0.18"
 URL = "https://api.sunrisesunset.io/json?"
 GEOCODING_URL = "https://geocoding-api.open-meteo.com/v1/search"
 LOCATION = "Bydgoszcz"
@@ -14,22 +15,29 @@ LONGITUDE = "18.008438" # Bydgoszcz longitude
 class Application(Tk):
     def __init__(self):
         super().__init__()
-        self.bulb = Bulb(BULB_IP)
         self.title("Yeelight Bulb Manager")
-        # self.location = LOCATION
-        self.latitude = LATITUDE
-        self.longitude = LONGITUDE
+        config_data: dict[str, str | int] = self.load_config()
+        self.bulb: Bulb = Bulb(config_data.get("bulb_ip", BULB_IP))
+        self.latitude: str = str(config_data.get("latitude", LATITUDE))
+        self.longitude: str = str(config_data.get("longitude", LONGITUDE))
+        self.location = StringVar(value=str(config_data.get("location", LOCATION)))
+        self.auto_on_var = IntVar(value=int(config_data.get("auto_on_var", 0)))
+        self.offset = StringVar(value=str(config_data.get("offset", "0")))
+        self.auto_off_var = IntVar(value=int(config_data.get("auto_off_var", 0)))
+        self.off_time = StringVar(value=str(config_data.get("off_time", "00:00")))
+        self.exit_var = IntVar(value=int(config_data.get("exit_var", 0)))
+
         self.vcmd = (self.register(self.validate), "%P")
         self.errmsg = StringVar()
         self.create_widgets()
         self.bind(sequence="<Escape>", func=self.exit)
     
-    def create_widgets(self):
+    def create_widgets(self) -> None:
         # Create mainframe
         self.mainframe = ttk.Frame(self, padding="5")
         self.mainframe.grid(column=0, row=0, sticky="n s e w")
 
-        # Bulb state text label
+        # Bulb state image label
         self.bulb_state = StringVar()
         self.img_bulb_on = PhotoImage(file="bulb_on.gif")
         self.img_bulb_off = PhotoImage(file="bulb_off.gif")
@@ -47,8 +55,8 @@ class Application(Tk):
         ttk.Label(self.mainframe, textvariable=self.time).grid(column=1, row=1)
 
         # Location text label
-        self.location = StringVar()
-        self.location.set(LOCATION)
+        # self.location = StringVar()
+        # self.location.set(LOCATION)
         ttk.Label(self.mainframe, text="Location:").grid(column=0, row=2)
         ttk.Label(self.mainframe, textvariable=self.location).grid(column=1, row=2)
 
@@ -59,13 +67,14 @@ class Application(Tk):
         ttk.Label(self.mainframe, textvariable=self.sunset).grid(column=1, row=3)
 
         # Auto-on at sunset checkbutton
-        self.auto_on_check: ttk.Checkbutton = ttk.Checkbutton(self.mainframe, text="Auto-on at sunset", command=self.sunset_turn_on)
+        # self.auto_on_var = IntVar(value=0)
+        self.auto_on_check: ttk.Checkbutton = ttk.Checkbutton(self.mainframe, text="Auto-on at sunset", command=self.sunset_turn_on, variable=self.auto_on_var)
         self.auto_on_check.grid(column=0, row=4)
 
         # Auto-on time offset spinbox
         ttk.Label(self.mainframe, text="Set time offset:").grid(column=0, row=5)
-        self.offset = StringVar()
-        self.offset.set("0")
+        # self.offset = StringVar()
+        # self.offset.set("0")
         self.spnbox = ttk.Spinbox(self.mainframe, from_=-60.0, to=60.0, increment=10.0, textvariable=self.offset, command=self.calculate_turn_on_time, wrap=True, width=3)
         self.spnbox.state(["readonly"])
         if not self.auto_on_check.instate(["selected"]):
@@ -74,13 +83,14 @@ class Application(Tk):
         ttk.Label(self.mainframe, text="minutes").grid(column=2, row=5, sticky='w')
 
         # Auto-off checkbutton
-        self.auto_off_check = ttk.Checkbutton(self.mainframe, text="Auto-off", command=self.auto_off)
+        # self.auto_off_var = IntVar(value=0)
+        self.auto_off_check = ttk.Checkbutton(self.mainframe, text="Auto-off", command=self.auto_off, variable=self.auto_off_var)
         self.auto_off_check.grid(column=0, row=7)
 
         # Auto-off time combobox
         ttk.Label(self.mainframe, text="Set auto-off time:").grid(column=0, row=8)
-        self.off_time = StringVar()
-        self.off_time.set("00:00")
+        # self.off_time = StringVar()
+        # self.off_time.set("00:00")
         self.cmbbox = ttk.Combobox(self.mainframe, textvariable=self.off_time, width=5)
         self.cmbbox["values"] = [
                             str(h)+":0"+str(m)
@@ -96,7 +106,8 @@ class Application(Tk):
         self.cmbbox.grid(column=1, row=8)
 
         # Exit auto-off checkbutton
-        self.ext_auto_off_check = ttk.Checkbutton(self.mainframe, text="Auto-off at exit")
+        # self.exit_var = IntVar(value=0)
+        self.ext_auto_off_check = ttk.Checkbutton(self.mainframe, text="Auto-off at exit", variable=self.exit_var)
         self.ext_auto_off_check.grid(column=0, row=9)
 
         # Location entry
@@ -109,19 +120,22 @@ class Application(Tk):
         self.loc_btn.state(["disabled"])
         self.loc_btn.grid(column=3, row=4)
 
+        # Save configuration button
+        ttk.Button(self.mainframe, text="Save config", command=self.save_config).grid(column=2, row=10)
+
         # Exit button
         ttk.Button(self.mainframe, text="Exit", command=self.exit).grid(column=3, row=10)
 
-    def toggle_bulb(self):
+    def toggle_bulb(self) -> None:
         self.bulb.toggle()
         self.set_bulb_state()
     
-    def get_sunset(self):
+    def get_sunset(self) -> None:
         payload: dict[str, str] = {'lat': self.latitude, 'lng': self.longitude, 'time_format': '24'}
         response: requests.Response = requests.get(url=URL, params=payload)
         self.sunset.set(response.json()['results']['sunset'][:5])
     
-    def sunset_turn_on(self):
+    def sunset_turn_on(self) -> None:
         if self.auto_on_check.instate(["selected"]):
             self.spnbox.state(["!disabled"])
             print("Sunset turn on enabled.")
@@ -131,7 +145,7 @@ class Application(Tk):
             self.spnbox.state(["disabled"])
             print("Sunset turn on disabled.")
     
-    def sunset_turn_on_loop(self):
+    def sunset_turn_on_loop(self) -> None:
         if self.time_turn_on == self.time.get():
             if self.bulb_state.get() == "off":
                 self.bulb.turn_on()
@@ -140,7 +154,7 @@ class Application(Tk):
         if self.auto_on_check.instate(["selected"]):
             self.after(1000, self.sunset_turn_on_loop)
     
-    def calculate_turn_on_time(self):
+    def calculate_turn_on_time(self) -> None:
         sset: str = self.sunset.get()
         ofst = int(self.offset.get())
         if int(sset[3:]) < abs(int(ofst)):
@@ -157,11 +171,11 @@ class Application(Tk):
                 minutes -= 60
         self.time_turn_on: str = str(hours)+":"+(str(minutes) if minutes >= 10 else "0"+str(minutes))
 
-    def time_update(self):
+    def time_update(self) -> None:
         self.time.set(datetime.now().strftime("%H:%M"))
         self.after(1000, self.time_update)
     
-    def auto_off(self):
+    def auto_off(self) -> None:
         if self.auto_off_check.instate(["selected"]):
             self.cmbbox.state(["!disabled"])
             print("Auto turn-off enabled.")
@@ -170,7 +184,7 @@ class Application(Tk):
             self.cmbbox.state(["disabled"])
             print("Auto turn-off disabled.")
     
-    def auto_off_loop(self):
+    def auto_off_loop(self) -> None:
         if self.off_time.get() == self.time.get():
                 if self.bulb_state.get() == "on":
                     self.bulb.turn_off()
@@ -179,35 +193,30 @@ class Application(Tk):
         if self.auto_off_check.instate(["selected"]):
             self.after(1000, self.auto_off_loop)
     
-    def exit(self, *args):
+    def exit(self, *args) -> None:
         if self.ext_auto_off_check.instate(["selected"]):
             self.bulb.turn_off()
         self.destroy()
 
-    def set_bulb_state(self):
+    def set_bulb_state(self) -> None:
         self.bulb_state.set(self.bulb.get_properties()["power"])
         if self.bulb_state.get() == "on":
             self.state_label["image"] = self.img_bulb_on
         else:
             self.state_label["image"] = self.img_bulb_off
 
-    def set_location(self):
-        payload = {"name": self.user_input_loc.get(), "count": 1}
-        response = requests.get(url=GEOCODING_URL, params=payload)
-        print(response.status_code)
-        if response.status_code == 200:
+    def set_location(self) -> None:
+        payload: dict[str, str | int] = {"name": self.user_input_loc.get(), "count": 1}
+        response: requests.Response = requests.get(url=GEOCODING_URL, params=payload)
+
+        try:
             self.latitude: str = str(response.json()['results'][0]['latitude'])
             self.longitude: str = str(response.json()['results'][0]['longitude'])
             self.location.set(response.json()['results'][0]['name'])
             self.get_sunset()
-        elif response.status_code == 400:
-            print("HTTP response 400")
-            # wrong request - invalid location
-            pass
-        else:
-            # wrong request - somethin else went wrong
-            pass
-  
+        except KeyError:
+            self.errmsg.set("Location not found.")
+        
     def validate(self, new_entry: str) -> bool:
         self.errmsg.set("")
         valid = new_entry.isalpha() or new_entry == ""
@@ -218,7 +227,33 @@ class Application(Tk):
             self.errmsg.set("Only letters allowed.")
             return valid
     
+    def save_config(self) -> None:
+        config_data: dict[str, str | int] = {
+            "location": self.location.get(),
+            "longitude": self.longitude, # do something with these, they're taken from constants
+            "latitude": self.latitude, # do something with these, they're taken from constants
+            "bulb_ip": BULB_IP, # add bulb_ip variable
+            "auto_on_var": self.auto_on_var.get(),
+            "offset": self.offset.get(),
+            "auto_off_var": self.auto_off_var.get(),
+            "off_time": self.off_time.get(),
+            "exit_var": self.exit_var.get()
+        }
 
+        with open("config.json", "w") as file:
+            json.dump(config_data, file)
+
+        print("Configuration saved.")
+
+    def load_config(self) -> dict[str, str | int]:
+        try:
+            with open("config.json", "r") as file:
+                config_data: dict[str, str | int] = json.load(file)
+            return config_data
+        except FileNotFoundError:
+            print("Config file not found. Loading defaults.")
+            return {}
+        
 if __name__ == '__main__':
     app = Application()
     app.mainloop()
